8997787c5be8095365fee726558cac50ac51f0d2
nverba <distantstate@gmail.com>
2015-08-24 13:54:44 +0100

    basic min/max working

--- src/js/mis-button.js
+++ src/js/mis-button.js
@@ -1,4 +1,6 @@
-var popup = `
+(function() { 'use strict';
+	
+	let popup = `
 		<div class="misFrame">
 			<div class="misSpinner">
 				<div class="circle1"></div>
@@ -7,78 +9,77 @@ var popup = `
 				<p class="loadingText">Loading</p>
 				<a class="mis-faq" href="https://makeitsocial.com/faq/" target="_blank">Learn more</a>
 			</div>
-			<div class="misToggle misTogMax"></div>
+			<div class="misIco misMin"></div>
 		</div>
 	  <iframe class="MiS_PopUp" src="" style="display:none"></iframe>
-`
-// <div class="misFrameBlock" style="position: absolute;  height: 450px; width:400px; z-index: 20004;display: none;"></div>
-
-var buttons = document.getElementsByClassName("makeitsocial-button");
-var isMobile = navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
-
-// Create & append the container for opened, minimised popups.
-var misOpen = document.createElement('div');
-misOpen.className = "misOpen";
-document.body.appendChild(misOpen);
-
-for (var i = 0; i < buttons.length; i++) {
+	`
 	
-	let element = buttons[i];
-	let maximised = true;
-	let misOpened = false;
+	let buttons = document.getElementsByClassName("makeitsocial-button");
+	let isMobile = navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
 	
-	element.addEventListener('click', function() {
+	// Create & append the container for opened, minimised popups.
+	let minimised = document.createElement('div');
+	minimised.className = "misMinimised";
+	document.body.appendChild(minimised); 
+	
+	for (let i = 0; i < buttons.length; i++) {
 		
-		if (isMobile) {
-			window.location.href = "https://popup-sandbox.herokuapp.com#/setup?pid=" + element.getAttribute('data-pid');
-			return;
+		let button = buttons[i];
+		let active = false;
+		let pid = button.getAttribute('data-pid');
+		
+		let maximise = document.createElement('button');
+		maximise.className = "misBtn";
+		maximise.innerText = "LINK";
+		minimised.appendChild(maximise);
+		
+		let container = document.createElement('div');
+		container.className = "misCover";
+		
+		function launchPopup(e) {
+			
+			if (isMobile) {
+				window.location.href = "https://popup-sandbox.herokuapp.com#/setup?pid=" + pid;
+				return;
+			}
+			
+			// Open only one popup per button;
+			if (active) { 
+				maximisePopup();
+				return;
+			}
+			
+			active = true;
+			container.innerHTML = popup;
+			document.body.appendChild(container);
+			
+			let loader = container.getElementsByClassName('misSpinner')[0];
+			let iframe = container.getElementsByClassName('MiS_PopUp')[0];
+			let minimi = container.getElementsByClassName('misMin')[0];
+			
+			iframe.onload = function(e) {
+				// Give 1s to let page render with product details
+				setTimeout(function() {
+					loader.style.display = "none";
+					iframe.style.display = "block";
+				}, 1000);
+			};
+			
+			function minimisePopup() {
+				container.className = "misCover misMoveMin";
+				maximise.className  = "misBtn mimimised";
+			}
+			
+			function maximisePopup() {
+				container.className = "misCover";
+				maximise.className  = "misBtn";
+			}
+			maximise.addEventListener('click', maximisePopup);
+			minimi.addEventListener('click', minimisePopup);
+			iframe.setAttribute('src', "https://popup-sandbox.herokuapp.com#/setup?pid=" + pid);
 		}
 		
-		// Open only one popup per button;
-		if (misOpened) { return; }
-		misOpened = true;
+		button.addEventListener('click', launchPopup);
 		
-		// create the popup container, and append to the document.
-		var container = document.createElement('div');
-		container.className = "misCover";
-		container.innerHTML = popup;
-		document.body.appendChild(container);
-		
-		let loader = container.getElementsByClassName('misSpinner')[0];
-		let iframe = container.getElementsByClassName('MiS_PopUp')[0];
-		let toggle = container.getElementsByClassName('misToggle')[0];
-		
-		iframe.onload = function() {
-			// Give 1s to let page render with product details
-			setTimeout(function() {
-				loader.style.display = "none";
-				iframe.style.display = "block";
-			}, 1000);
-		};
-		
-		toggle.addEventListener('click', function() {
-			
-			if (maximised) {
-				container.className = "misCover misMoveMin";
-				setTimeout(function() {
-					toggle.className = "misToggle misTogMin"; // Toggle button style
-					container.className = "misCover misMin";  // Toggle container style
-					misOpen.appendChild(container);						// Append to opend popups container
-					setTimeout(function() {
-						container.className = "misCover misMin misShow";  // Toggle container style
-					}, 0);
-				}, 450);	
-			} else {
-				toggle.className = "misToggle misTogMax";
-				container.className = "misCover";
-				document.body.appendChild(container);     // Return popup to body
-			}
-			maximised = !maximised;
-		});
-		
-		iframe.setAttribute('src', "https://popup-sandbox.herokuapp.com#/setup?pid=" + element.getAttribute('data-pid'));
-				
-	});
-}
-
-
+	}
+})()
